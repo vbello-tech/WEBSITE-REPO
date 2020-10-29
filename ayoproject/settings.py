658187fdd4632afaf5970e33f05c8609e3ae8c47
vbello-tech <odunayobello1@gmail.com>
2020-10-29 03:37:10 -0700

    decouple

--- ayoproject/settings.py
+++ ayoproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import django_heroku
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=7r886$d3ur@hpe6(m*4ez3y5pkjaz!b61nw8qx*p2r7@g*3em'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
